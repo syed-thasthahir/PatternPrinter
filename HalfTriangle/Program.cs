26ab021123f71a881652918061ee3665fd423a5d
syed thasthahir <syed1104@outlook.com>
2021-08-13 18:21:26 +0530

    Added New Pattern Technique

--- HalfTriangle/Program.cs
+++ HalfTriangle/Program.cs
@@ -7,8 +7,8 @@ namespace PrintingShapes
         static void Main(string[] args)
         {
             Console.WriteLine("Enter Respective Rows");
-            int n = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < n; ++i)
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; ++i)
             {
                 for (int j = 0; j <= i; ++j)
                 {
--- HalfTriangleWithCombinationOfSymbol/Program.cs
+++ HalfTriangleWithCombinationOfSymbol/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace HalfTriangleWithCombinationOfSymbol
+namespace PrintingShapes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Respective Rows");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The Symbol");
+            string symbol1 = Console.ReadLine();
+            Console.WriteLine("Enter The Symbol");
+            string symbol2 = Console.ReadLine();
+            for (int i = 1; i <= number; ++i)
+            {
+                for (int j = 1; j <= i; ++j)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(symbol1);
+                    }
+                    else
+                    {
+                        Console.Write(symbol2);
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
